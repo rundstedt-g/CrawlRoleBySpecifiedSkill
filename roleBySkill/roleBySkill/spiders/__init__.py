84368d18ecca9756ea4b5a218ac3f0077bf03949
王雄 <6874024@qq.com>
2021-08-06 11:06:28 +0800

    feat: 出售区搜索

--- roleBySkill/roleBySkill/spiders/__init__.py
+++ roleBySkill/roleBySkill/spiders/__init__.py
@@ -12,41 +12,52 @@ from ..items import RolebyskillItem
 
 class RolebyskillSpider(scrapy.Spider):
     name = 'roleBySkill'
-    url = 'http://jishi.woniu.com/9yin/anonymous/findNoticeGoods.do?serverId=186100101&gameId=10'
+    url1 = 'http://jishi.woniu.com/9yin/anonymous/findSellingGoods.do?serverId=186100101&gameId=10&auctionFirst=1&filterItem=4'
+    url2 = 'http://jishi.woniu.com/9yin/anonymous/findNoticeGoods.do?serverId=186100101&gameId=10&auctionFirst=1&filterItem=4'
     time_stamp = str(int(time.time()*1000))
     allowed_domains = ['woniu.com']
-    start_urls = [url+'&filterItem=4&_='+time_stamp]
+    start_urls = [url1 + '&_=' + time_stamp,url2 + '&_=' + time_stamp]
 
-    def _parse(self, response):
-        sites = json.loads(response.body_as_unicode())
+    def parse(self, response):
+        print('初始请求：' + response.url)
+        sites = json.loads(response.text)
         totalPages = sites[0]['pageInfo']['totalPages']
         for i in range(1,totalPages+1):
-            new_time_stamp = str(int(time.time()*1000))
-            newUrl = self.url + '&pageIndex=' +str(i) +'&filterItem=4&_=' + new_time_stamp
+            new_time_stamp = '&_=' + str(int(time.time()*1000))
+            if response.url.find('Selling') != -1 :
+                newUrl = self.url1 + '&pageIndex=' +str(i) + new_time_stamp
+            else:
+                newUrl = self.url2 + '&pageIndex=' +str(i) + new_time_stamp
+            print('目标请求：' + newUrl)
             yield scrapy.Request(newUrl, callback=self.parse_content)
 
     def parse_content(self, response):
-        sites = json.loads(response.body_as_unicode())
+        print('分页请求：' + response.url)
+        sites = json.loads(response.text)
         for i in sites[0]['pageData']:
             roleUid = i['sellerGameId']
-            print(roleUid)
 
             newSkill_time_stamp = str(int(time.time()*1000))
             skillUrl = 'http://jishi.woniu.com/9yin/roleMsg.do?serverId=186100101&roleUid='+roleUid+'&type=SkillContainer&_='+newSkill_time_stamp
-            print(skillUrl)
-            yield scrapy.Request(skillUrl, callback=self.parse_skillcontent,meta={'name':i['itemName'],'id':i['id'],'price':i['price']})
+            yield scrapy.Request(skillUrl, callback=self.parse_skillcontent,meta={'name':i['itemName'],'id':i['id'],'price':i['price'],'status':i['status']})
 
     def parse_skillcontent(self, response):
-        sites = json.loads(response.body_as_unicode())
+        print('技能请求：' + response.url)
+        sites = json.loads(response.text)
         msg = json.loads(sites[0]['msg'])
         flag = False
         for i in msg:
-            if i['type']=="双刺套路/伏月刺诀" :
+            if i['type']=="江湖散招/江湖散手" and i['dataInfo'].find("神行百变")!=-1:
                 flag=True
                 break
+        print(response.meta['name'] + ' ' + str(flag))
         if flag == True:
             item = RolebyskillItem()
             item['name'] = response.meta['name']
             item['id'] = response.meta['id']
             item['price'] = response.meta['price']
+            if response.meta['status'] == '2':
+                item['status'] = '公示期'
+            else:
+                item['status'] = response.meta['status']
             yield item
